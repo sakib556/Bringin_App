72cd1127542eb1b18bda4f979080c7f0d0afb0d8
Sakib <kazisakib556@gmail.com>
2023-02-08 11:19:47 +0600

    app structure created

---
+++ lib/constants/api_constant.dart
@@ -0,0 +1,5 @@
+const baseUrl = "https://bringin.io/api";
+const sendOtpUrl = "$baseUrl/sendOTP";
+const getTokenUrl = "$baseUrl/setLoginWithOTP";
+const seekerProfileUpdate = "$baseUrl/seekerProfileUpdate";
+const getSeekerInfo = "$baseUrl/getSeekerInfo"; // need token
